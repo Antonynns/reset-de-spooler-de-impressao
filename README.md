c440771a202d92d31a854941d269f9b998846409
Antonynns <nunesantonio1111@gmail.com>
2024-08-29 10:16:22 -0300

    Serviço de reset de spooler funcionando

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # reset de spooler de impressao
  
- Automação desenvolvida para limpar impressões em cache na fila e resetar spooler de impressão
+    Automação desenvolvida para limpar impressões em cache na fila e resetar spooler de impressão
+
+    Necessário execução com permissões de administrador.
--- impressora.bat
+++ impressora.bat
@@ -1,3 +1,8 @@
 @echo off
-echo Olá, Mundo!
+echo Reiniciando o serviço de spooler de impressão, aguarde
+net stop spooler
+echo Limpando arquivos do spooler...
+del /Q /F /S "%systemroot%\System32\spool\PRINTERS\*.*"
+net start spooler
+echo Serviço de spooler de impressão reiniciado com sucesso!
 pause
